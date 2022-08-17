c7c2474b3744c0cccfa6c481a0431559bc27f91a
Vasua Pupg --global user.email you@gmail <you@gmail.com>
2022-08-17 14:29:05 +0300

    First commit

---
+++ Home.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Server.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+     <form id="form2" runat="server">
+         
+           <asp:ListBox ID="ListBox1" runat="server" Height="0px" Width="0px" ></asp:ListBox>
+         
+ 
+        <div style="text-align: center">         
+            HI<br/>
+            Здесь вы сможите узнать, зарегестрирован ли ваш пользователь в операционной системе   
+        </div>
+            
+        <br/><br/>
+        <div style="text-align: center">       
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+              </div> 
+           
+  <br/><br/>
+         <div style="text-align: center">       
+             <asp:Button ID="Button2" runat="server" OnClick="Button1_Click" Text="Проверить"  />            
+         </div>                      
+         
+        <div style="text-align: center">       
+              </div> 
+
+
+    </form>
+</body>
+</html>
---
+++ Home.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Server
+{
+
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            ManagementObjectSearcher searcher =
+        new ManagementObjectSearcher("root\\CIMV2",
+        "SELECT * FROM Win32_UserAccount WHERE LocalAccount=True");
+            foreach (ManagementObject envVar in searcher.Get())
+            {
+                ListBox1.Items.Add(envVar["Name"].ToString());
+
+            }
+        }
+
+        string str = "";
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ListBox1.Items.Count; i++)
+            {
+                if (ListBox1.Items[i].Text.ToString() == TextBox1.Text.ToString())
+                {
+                    str = ListBox1.Items[i].Text.ToString();
+                    break;
+                }
+            }
+
+            if (str != "")
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "error", "alert(\"пользователь '" + TextBox1.Text + "' найден!\")", true);
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this, GetType(), "error", "alert(\"пользователь '" + TextBox1.Text + "' не найден!\")", true);
+            }
+        }
+    }
+}
